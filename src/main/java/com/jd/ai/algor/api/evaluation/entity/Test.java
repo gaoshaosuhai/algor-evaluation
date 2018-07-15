bc8ee041121b07d84d24030220574e36f54ee889
gaossb <gaossb@yonyou.com>
2018-07-15 18:03:09 +0800

    mysql test

---
+++ src/main/java/com/jd/ai/algor/api/evaluation/entity/Test.java
@@ -0,0 +1,12 @@
+package com.jd.ai.algor.api.evaluation.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Test {
+    private Long id;
+
+    private String code;
+}
--- src/test/java/com/jd/ai/algor/api/evaluation/AlgorApiEvaluationApplicationTests.java
+++ src/test/java/com/jd/ai/algor/api/evaluation/AlgorApiEvaluationApplicationTests.java
@@ -7,25 +7,37 @@ import org.redisson.api.RStream;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AlgorApiEvaluationApplicationTests {
 
+//    @Autowired
+//    private RedissonClient redissonClient;
+
     @Autowired
-    private RedissonClient redissonClient;
+    private JdbcTemplate template;
 
     @Test
     public void contextLoads() {
     }
 
+//    @Test
+//    public void testRedission() {
+//        RBucket<String> bucket = redissonClient.getBucket("1111");
+//        String o = bucket.get();
+//        System.out.println(o);
+//    }
+
     @Test
-    public void testRedission() {
-        RBucket<String> bucket = redissonClient.getBucket("1111");
-        bucket.set("nihao");
-        String o = bucket.get();
-        System.out.println(o);
+    public void testMysql() {
+        List<com.jd.ai.algor.api.evaluation.entity.Test> query = template.query("select * from test", new BeanPropertyRowMapper<>(com.jd.ai.algor.api.evaluation.entity.Test.class));
+        System.out.println(query.size());
     }
 
 }
